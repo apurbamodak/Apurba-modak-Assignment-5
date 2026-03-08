e45d1d9afa1c2c64179f6c9538b4260b8fff2937
apurbamodak <apurbamodak01@gmail.com>
2026-03-08 18:09:10 +0530

    home pag script part done mostly

---
+++ script/script.js
@@ -0,0 +1,163 @@
+const issuesContainer = document.getElementById("issuesContainer");
+const loadingSpinner = document.getElementById("loadingSpinner");
+const searchInput = document.getElementById("searchInput");
+const allIssuesBtn = document.getElementById("allIssuesBtn");
+const openIssuesBtn = document.getElementById("openIssuesBtn");
+const closedIssuesBtn = document.getElementById("closedIssuesBtn");
+const issueCount = document.getElementById("issueCount");
+const issueModal = document.getElementById("issueModal");
+const modalTitle = document.getElementById("modalTitle");
+const modalDescription = document.getElementById("modalDescription");
+const modalAuthor = document.getElementById("modalAuthor");
+const modalPriority = document.getElementById("modalPriority");
+const modalCategory = document.getElementById("modalCategory");
+const modalStatusBadge = document.getElementById("modalStatusBadge");
+
+let allIssues = [];
+
+function showLoading() {
+    loadingSpinner.classList.remove("hidden");
+    issuesContainer.innerHTML = "";
+}
+
+function hideLoading() {
+    loadingSpinner.classList.add("hidden");
+}
+
+async function loadIssues() {
+    showLoading();
+    const res = await fetch(
+        "https://phi-lab-server.vercel.app/api/v1/lab/issues"
+    );
+    const data = await res.json();
+    allIssues = data.data;
+    displayIssues(allIssues);
+    hideLoading();
+}
+
+
+function displayIssues(issues) {
+    issuesContainer.innerHTML = "";
+    issueCount.textContent = `${issues.length} Issues`;
+    issues.forEach(issue => {
+        const card = document.createElement("div");
+        const borderColor =
+            issue.status === "open"
+                ? "border-green-500"
+                : "border-purple-500";
+        card.className =
+            `card bg-white shadow-sm border-t-4 ${borderColor}`;
+        card.innerHTML = `
+        <div class="card-body">
+        <div class="flex justify-end"> <span class="badge badge-warning">${issue.priority}</span> </div>
+        <h2
+        class="card-title cursor-pointer hover:text-blue-600"
+        data-id="${issue._id}"
+        >
+        ${issue.title}
+        </h2>
+        <p class="line-clamp-2 overflow:hidden">
+        ${issue.description}
+        </p>
+        <div class="flex flex-wrap gap-2 text-xs mt-2">
+        ${issue.labels.map(label => `
+            <span class="badge ${label === "bug"
+                ? "bg-red-100 text-red-600 border-red-200"
+                : "bg-yellow-100 text-yellow-700 border-yellow-200"
+            } border">${label.toUpperCase()}</span>`
+        ).join("")}
+        </div>
+        <br>
+        <hr>
+        <p class="text-xs text-gray-400 mt-2">
+        ${issue.author}
+        </p>
+        <p class="text-xs text-gray-400">
+        ${issue.createdAt}
+        </p>
+        </div>
+        `;
+
+        card.addEventListener("click", () => {
+            openIssueModal(issue._id);
+        });
+        issuesContainer.appendChild(card);
+    });
+
+}
+
+
+function filterIssues(status, btn) {
+    const buttons = [allIssuesBtn, openIssuesBtn, closedIssuesBtn];
+    buttons.forEach(button => {
+        button.classList.remove("btn-primary");
+        button.classList.add("btn-outline");
+    });
+    btn.classList.add("btn-primary");
+    btn.classList.remove("btn-outline");
+    if (status === "all") {
+        displayIssues(allIssues);
+        return;
+    }
+
+    const filtered =
+        allIssues.filter(issue => issue.status === status);
+    displayIssues(filtered);
+
+}
+
+
+async function searchIssues() {
+    const text = searchInput.value;
+    if (text === "") {
+        loadIssues();
+        return;
+    }
+    showLoading();
+
+    const res = await fetch(
+        `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${text}`
+    );
+    const data = await res.json();
+    displayIssues(data.data);
+    hideLoading();
+
+}
+
+
+
+async function openIssueModal(id) {
+    const res = await fetch(
+        `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+    );
+    const data = await res.json();
+    const issue = data.data;
+    modalTitle.textContent = issue.title;
+    modalDescription.textContent = issue.description;
+    modalAuthor.textContent = issue.author;
+    modalCategory.textContent = issue.category;
+    modalPriority.textContent = issue.priority;
+    modalStatusBadge.textContent = issue.status;
+    if (issue.status === "open") {
+        modalStatusBadge.className = "badge badge-success";
+    } else {
+        modalStatusBadge.className = "badge badge-secondary";
+    }
+    issueModal.showModal();
+}
+
+
+allIssuesBtn.addEventListener("click", () => {
+    filterIssues("all", allIssuesBtn);
+});
+
+openIssuesBtn.addEventListener("click", () => {
+    filterIssues("open", openIssuesBtn);
+});
+
+closedIssuesBtn.addEventListener("click", () => {
+    filterIssues("closed", closedIssuesBtn);
+});
+searchInput.addEventListener("keyup", searchIssues);
+
+loadIssues();
